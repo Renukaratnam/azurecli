f9148ffdc43ed71066313d4bf44de7336db02ced
Renukaratnam <renukabendadi@gmail.com>
2023-07-12 22:22:06 +0530

    network sg created

--- vnet.tf
+++ vnet.tf
@@ -23,3 +23,40 @@ resource "azurerm_subnet" "renu-sn" {
   address_prefixes     = ["10.0.1.0/24"]
 
 }
+
+# network security group
+
+resource "azurerm_network_security_group" "renu-nsg" {
+  name                = "tf-ssh-http"
+  location            = azurerm_resource_group.renu-rg.location
+  resource_group_name = azurerm_resource_group.renu-rg.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 500
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+security_rule {
+    name                       = "http"
+    priority                   = 510
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+  tags = {
+    environment = "dev"
+  }
+}
